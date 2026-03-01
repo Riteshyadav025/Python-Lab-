3be9c200aaa43078bb062977949353bcf2ec9d79
Ritesh Yadav <deveasy44@gmail.com>
2026-03-01 23:14:55 +0530

    Implement minimum jumps calculation function

---
+++ 09-02-2026 To 14-02-2026/minimum_jumps.py
@@ -0,0 +1,17 @@
+nums = [2, 3, 1, 1, 4]
+
+def minJumps(nums):
+    jumps = 0
+    current_end = 0
+    farthest = 0
+    
+    for i in range(len(nums) - 1):
+        farthest = max(farthest, i + nums[i])
+        
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+            
+    return jumps
+
+print("Minimum jumps:", minJumps(nums))
