5996ea3d38d9cfa61a8ae764587be6e7d96bc997
Ritesh Yadav <deveasy44@gmail.com>
2026-03-08 23:14:23 +0530

    Create median.py

---
+++ 02-02-2026 To 07-02-2026/median.py
@@ -0,0 +1,11 @@
+def findMedian(arr):
+    arr.sort()
+    n = len(arr)
+
+    if n % 2 == 1:
+        return arr[n // 2]
+    else:
+        return (arr[n//2 - 1] + arr[n//2]) / 2
+
+arr = [90, 100, 78, 89, 67]
+print(findMedian(arr))
